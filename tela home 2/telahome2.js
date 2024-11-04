04113aa211394bb1512d8c0d832cc8fd02661788
PedroHCruzz <pedro.geraldo@sptech.school>
2024-11-03 23:09:36 -0300

    polindo a tela2 da home

--- tela home 2/telahome2.js
+++ tela home 2/telahome2.js
@@ -1,19 +1,12 @@
-const $next = document.querySelector('.next');
-const $prev = document.querySelector('.next');
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
 
-$next.addEventListener(
-    'click',
-() => {
-    const items = document.querySelectorAll('.item');
-    document.querySelector('.slide').appendChild(items[0]);
- },
-);
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
 
-$prev.addEventListener (
-    'click',
-    () => {
-        const items = document.querySelectorAll('.item');
-
-        document.querySelector('.slide').prepend(items[items.length - 1]);
-    },
-);
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
