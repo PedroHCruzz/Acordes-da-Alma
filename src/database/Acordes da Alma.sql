002d4bbcfe46896f6b88af2cb22d5b0dffde7d6d
PedroHCruzz <pedro.geraldo@sptech.school>
2024-11-19 10:18:16 -0300

    tentando colocar a api

--- src/database/Acordes da Alma.sql
+++ src/database/Acordes da Alma.sql
@@ -1,43 +1,50 @@
 create database acordesdaalma;
 use acordesdaalma;
 
+-- drop database acordesdaalma;
+
 create table usuario (
-    idusuario int primary key auto_increment,
+    idUsuario int primary key auto_increment,
     nome varchar(45) not null,
     email varchar(45) not null unique,
     senha varchar(45) not null,
-    confirmar_senha varchar(45) not null,
-    data_cadastro date not null
+    confirmar_senha varchar(45) not null
 );
+
+select * from usuario;
 
 create table musica (
-    idmusica int primary key auto_increment,
+    idMusica int primary key auto_increment,
     titulo varchar(45) not null,
-    artista varchar(45) not null,
-    album varchar(45),
-    genero varchar(50),
-    duracao time
+    artista varchar(45) not null
 );
+
+select * from musica;
 
 create table emocao (
-    idemocao int primary key auto_increment,
-    descricao varchar(50) not null unique
+    idEmocao int primary key auto_increment,
+    descricao varchar(200) not null unique
 );
+
+select * from emocao;
 
 create table usuario_musica (
-    idusuario int,
-    idmusica int,
+    fkUsuario int,
+    fkMusica int,
     data_escolha date not null,
-    primary key (id_usuario, id_musica),
-    foreign key (id_usuario) references usuario(id_usuario) on delete cascade,
-    foreign key (id_musica) references musica(id_musica) on delete cascade
+    primary key (fkUsuario, fkMusica),
+    foreign key (fkUsuario) references usuario(idUsuario) on delete cascade,
+    foreign key (fkMusica) references musica(idMusica) on delete cascade
 );
 
+select * from usuario_musica;
 
 create table musica_emocao (
-    id_musica int,
-    id_emocao int,
-    primary key (id_musica, id_emocao),
-    foreign key (id_musica) references musica(id_musica) on delete cascade,
-    foreign key (id_emocao) references emocao(id_emocao) on delete cascade
+    fkMusica int,
+    fkEmocao int,
+    primary key (fkMusica, fkEmocao),
+    foreign key (fkMusica) references musica(idMusica),
+    foreign key (fkEmocao) references emocao(idEmocao)
 );
+
+select * from musica_emocao;
