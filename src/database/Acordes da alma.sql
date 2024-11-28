4b888f5e6825049df00da13004018198f74e416f
PedroHCruzz <pedro.geraldo@sptech.school>
2024-11-27 21:25:59 -0300

    mudando o script do banco de dados

--- src/database/Acordes da alma.sql
+++ src/database/Acordes da alma.sql
@@ -11,8 +11,8 @@ create table usuario (
     confirmar_senha varchar(45) not null
 );
 
- INSERT INTO usuario (nome, email, senha, confirmar_senha) VALUES
- ('zé', 'ze@gmail.com', '1234', '1234');
+--  INSERT INTO usuario (nome, email, senha, confirmar_senha) VALUES
+--  ('zé', 'ze@gmail.com', '1234', '1234');
 
 select * from usuario;
 
@@ -24,11 +24,11 @@ create table musica (
 
 select * from musica;
 
-SELECT nome, email, senha FROM usuario WHERE email = 'ze@gmail.com' AND senha = '1234';
+-- SELECT nome, email, senha FROM usuario WHERE email = 'ze@gmail.com' AND senha = '1234';
 
 create table emocao (
     idEmocao int primary key auto_increment,
-    descricao varchar(200) not null unique
+    descricao varchar(500) not null unique
 );
 
 select * from emocao;
@@ -51,5 +51,23 @@ create table musica_emocao (
     foreign key (fkMusica) references musica(idMusica),
     foreign key (fkEmocao) references emocao(idEmocao)
 );
+
+create table votos (
+    idVotos int primary key,          
+    fkusuario int not null,   
+    foreign key (fkusuario) references usuario(idUsuario),
+    opcao int not null,        
+    data_voto TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table comentarios (
+idComentario int primary key,
+fkusuario int,
+foreign key (fkusuario) references usuario(idUsuario),
+fkemocao int,
+foreign key (fkemocao) references emocao(idEmocao),
+conteudo varchar(500)
+);
+
 
 select * from musica_emocao;
